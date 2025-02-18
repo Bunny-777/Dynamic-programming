7c1bb4f30be2543d98134dc7184ed5b71e07592d
Bunny <bunny202410@gmail.com>
2025-02-18 10:35:14 +0530

    fibo using dp

--- fibonnaci.cpp
+++ fibonnaci.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int func(int n,vector<int>& dp){
+    if(n<=1){
+        return n;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    return dp[n]=func(n-1,dp)+func(n-2,dp);
+}
 int main(){
-    cout<<"this is for testing purpose";
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    vector<int> dp(n+1,-1);
+    cout<<func(n,dp);
     return 0;
 }
