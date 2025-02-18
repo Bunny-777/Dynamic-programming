94a6388a777d5d870b3a7d54f274bd7619c40354
Bunny <bunny202410@gmail.com>
2025-02-18 18:40:58 +0530

    tabulation method

--- 3-frog_jump.cpp
+++ 3-frog_jump.cpp
@@ -24,27 +24,48 @@ using namespace std;
 // }
 
 // memoization approach (stroing values in dp array) having O(N) time complexity and O(n)+O(n) space complexity
-int func(int i,int n,vector<int> & vec,vector<int> & dp){
-    if(i==n-1){
-        return 0;
-    }
-    if(dp[n]!=-1){
-        return dp[n];
-    }
-    int l = INT_MAX, r = INT_MAX; 
-    if(i+1<n){
-        l=func(i+1,n,vec,dp)+abs(vec[i+1]-vec[i]);
-    }
-    if(i+2<n){
-        r=func(i+2,n,vec,dp)+abs(vec[i+2]-vec[i]);
-    }
-    dp[n]=min(l,r);
-    return dp[n];
-}
+// int func(int i,int n,vector<int> & vec,vector<int> & dp){
+//     if(i==n-1){
+//         return 0;
+//     }
+//     if(dp[i]!=-1){
+//         return dp[n];
+//     }
+//     int l = INT_MAX, r = INT_MAX; 
+//     if(i+1<n){
+//         l=func(i+1,n,vec,dp)+abs(vec[i+1]-vec[i]);
+//     }
+//     if(i+2<n){
+//         r=func(i+2,n,vec,dp)+abs(vec[i+2]-vec[i]);
+//     }
+//     dp[i]=min(l,r);
+//     return dp[n];
+// }
+
+// int main(){
+//     vector<int> vec={30, 20, 50, 10, 40};
+//     int n=vec.size();
+//     vector<int> dp(n+1,-1);
+//     cout<<func(0,n,vec,dp);
+// }
+
+
+
+// tabulation method which only use O(n) time complexity and O(1) space complexity
 
 int main(){
     vector<int> vec={30, 20, 50, 10, 40};
     int n=vec.size();
-    vector<int> dp(n+1,-1);
-    cout<<func(0,n,vec,dp);
+    vector<int> dp(n,-1);
+    dp[0]=0;
+    for(int i=1;i<n;i++){
+        int fs=dp[i-1]+abs(vec[i]-vec[i-1]);
+        int ss=INT_MAX;
+        if(i>1){
+             ss=dp[i-2]+abs(vec[i]-vec[i-2]);
+        }
+        dp[i]=min(fs,ss);
+    }
+    cout<<dp[n-1];
+    
 }
