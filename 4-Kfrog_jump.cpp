151a1a21b84e8026fc9e6e56b7c0a266ede5511c
Bunny <bunny202410@gmail.com>
2025-02-22 21:42:02 +0530

    memoizatoin approach

--- 4-Kfrog_jump.cpp
+++ 4-Kfrog_jump.cpp
@@ -1,22 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 // most basic approach using the recursion 
-int func(int i,int n,vector<int> & vec,int k){
+
+
+// int func(int i,int n,vector<int> & vec,int k){
+//     if(i==n-1){
+//         return 0;
+//     }
+//     int x=INT_MAX;
+//     for(int ind=1;ind<=k;ind++){
+//         if(i+ind<n){
+//             x=min(x,func(i+ind,n,vec,k)+abs(vec[i+ind]-vec[i]));
+//         }
+//     }
+//     return x;
+// }
+
+// int main(){
+//     vector<int> vec={10, 20,10};
+//     int k=1;
+//     int n=vec.size();
+//     cout<< func(0,n,vec,k);
+// }
+
+
+// using the memoization approach
+
+int func(int i,int n,vector<int> & dp,vector<int> & vec,int k){
     if(i==n-1){
         return 0;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
     }
     int x=INT_MAX;
     for(int ind=1;ind<=k;ind++){
         if(i+ind<n){
-            x=min(x,func(i+ind,n,vec,k)+abs(vec[i+ind]-vec[i]));
+            x=min(x,func(i+ind,n,dp,vec,k)+abs(vec[i+ind]-vec[i]));
         }
     }
-    return x;
+    dp[i]=x;
+    return dp[i];
 }
 
 int main(){
     vector<int> vec={10, 20,10};
     int k=1;
     int n=vec.size();
-    cout<< func(0,n,vec,k);
+    vector<int> dp(n+1,-1);
+    cout<< func(0,n,dp,vec,k);
 }
