70a5be070a3112ada88bc37e6713c9eb10b72185
Bunny <bunny202410@gmail.com>
2025-02-22 21:51:50 +0530

    using the tabulaion method

--- 4-Kfrog_jump.cpp
+++ 4-Kfrog_jump.cpp
@@ -26,27 +26,47 @@ using namespace std;
 
 // using the memoization approach
 
-int func(int i,int n,vector<int> & dp,vector<int> & vec,int k){
-    if(i==n-1){
-        return 0;
-    }
-    if(dp[i]!=-1){
-        return dp[i];
-    }
-    int x=INT_MAX;
-    for(int ind=1;ind<=k;ind++){
-        if(i+ind<n){
-            x=min(x,func(i+ind,n,dp,vec,k)+abs(vec[i+ind]-vec[i]));
-        }
-    }
-    dp[i]=x;
-    return dp[i];
-}
+// int func(int i,int n,vector<int> & dp,vector<int> & vec,int k){
+//     if(i==n-1){
+//         return 0;
+//     }
+//     if(dp[i]!=-1){
+//         return dp[i];
+//     }
+//     int x=INT_MAX;
+//     for(int ind=1;ind<=k;ind++){
+//         if(i+ind<n){
+//             x=min(x,func(i+ind,n,dp,vec,k)+abs(vec[i+ind]-vec[i]));
+//         }
+//     }
+//     dp[i]=x;
+//     return dp[i];
+// }
+
+// int main(){
+//     vector<int> vec={10, 20,10};
+//     int k=1;
+//     int n=vec.size();
+//     vector<int> dp(n+1,-1);
+//     cout<< func(0,n,dp,vec,k);
+// }
+
+
+// using the tabulation method 
 
 int main(){
-    vector<int> vec={10, 20,10};
-    int k=1;
+    vector<int> vec={10, 30, 40, 50, 20};
+    int k=3;
     int n=vec.size();
-    vector<int> dp(n+1,-1);
-    cout<< func(0,n,dp,vec,k);
+    vector<int> dp(n+1,INT_MAX);
+    dp[0]=0;
+    for(int i=1;i<n;i++){
+        for(int ind=1;ind<=k;ind++){
+            if(i-ind>=0){
+                dp[i]=min(dp[i],dp[i-ind]+abs(vec[i]-vec[i-ind]));
+            }
+        }
+    }
+    cout<<dp[n-1];
+    return 0;
 }
